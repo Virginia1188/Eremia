13326986d190c0308e9bb94b41a551f517ab5ae9
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-12-02 23:33:39 +0200

    Add navigation for logged in users

--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../../contexts/authContext";
 
 export default function Topbar() {
+
+    const {
+        isAuthenticated,
+        username
+    } = useContext(AuthContext)
     return (
         <div>
             <div className="container-fluid bg-primary py-3 d-none d-md-block">
@@ -8,14 +15,27 @@ export default function Topbar() {
                     <div className="row">
                         <div className="col-md-6 text-center text-lg-left mb-2 mb-lg-0">
                             <div className="d-inline-flex align-items-center">
-                            <Link className="text-white pr-3" to="/register">Регистрация</Link>
-                                <span className="text-white">|</span>
-                                <Link className="text-white px-3" to="/login">Вход</Link>
-                                <a className="text-white pr-3" href="">Моята Зала</a>
-                                <span className="text-white">|</span>
-                                <a className="text-white px-3" href="">Любими</a>
-                                <span className="text-white">|</span>
-                                <a className="text-white pl-3" href="">Профил</a>
+
+                                {!isAuthenticated && (
+                                    <div>
+                                        <Link className="text-white pr-3" to="/register">Регистрация</Link>
+                                        <span className="text-white">|</span>
+                                        <Link className="text-white px-3" to="/login">Вход</Link>
+                                    </div>
+                                )}
+
+                                {isAuthenticated && (
+                                    <div>
+                                        <Link className="text-white pr-3" href="">Моята Зала</Link>
+                                        <span className="text-white">|</span>
+                                        <Link className="text-white px-3" href="">Любими</Link>
+                                        <span className="text-white">|</span>
+                                        <Link className="text-white px-3" href="">Профил</Link>
+                                        <span className="text-white">|</span>
+                                        <Link className="text-white pl-3" href="">Изход</Link>
+                                    </div>
+                                )}
+
                             </div>
                         </div>
                         <div className="col-md-6 text-center text-lg-right">
