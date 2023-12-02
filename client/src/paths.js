2ab94c7d087e8ea02d6360969d4779a74192d057
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-12-02 23:56:52 +0200

    create paths

---
+++ client/src/paths.js
@@ -0,0 +1,13 @@
+
+const Path = {
+    Home: '/',
+    About: '/about',
+    Login: '/login',
+    Logout: 'logout',
+    Register: '/register',
+    Schedule: '/schedule',
+    Studios: '/studios',
+
+}
+
+export default Path;
