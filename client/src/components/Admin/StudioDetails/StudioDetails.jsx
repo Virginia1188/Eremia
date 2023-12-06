4e72ea8405f5da8bb534efc17735e1a83f4f5c97
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-12-06 10:53:41 +0200

    fix styles for studio details

--- client/src/components/Admin/StudioDetails/StudioDetails.jsx
+++ client/src/components/Admin/StudioDetails/StudioDetails.jsx
@@ -10,12 +10,14 @@ export default function StudioDetails() {
 
             <h2 className={styles.titleStudios} >Зала Младост</h2>
             <div className={styles.studioDetails}>
-                <p>address</p>
-                <span>Instructor</span>
+                <p>св. кипрян 254</p>
+                <span>Цветомир</span>
             </div>
+            <h4>Групи в зала Младост</h4>
 
 
-            <div className={styles.tableDiv}>
+            {/* TODO add schedule */}
+            {/* <div className={styles.tableDiv}>
                 <h5>График на репетициите</h5>
                 <Table responsive="sm" className={styles.table}>
                     <thead>
@@ -50,33 +52,49 @@ export default function StudioDetails() {
 
                     </tbody>
                 </Table>
-            </div>
-
+            </div> */}
 
             <div className={styles.cards}>
 
 
                 <div className={styles.card} >
-                    {/* <img className={styles.cardImg} src='../../../../public/img/logo_orange.png' alt="logo" /> */}
+                    <img className={styles.cardImg} src='https://scontent-lhr8-1.xx.fbcdn.net/v/t39.30808-6/384220642_705558081613582_1456725998677837583_n.jpg?stp=c52.0.206.206a_dst-jpg_p206x206&_nc_cat=111&ccb=1-7&_nc_sid=3d9721&_nc_ohc=CxfqeDrdzC8AX8TRgzM&_nc_ht=scontent-lhr8-1.xx&oh=00_AfCAcVS0V0HDVMAIqwl6JIpudgl4pcHlCmv69fdHKm1aIw&oe=657380F4' alt="logo" />
                     <div >
                         <h2 className={styles.cardH2}>
-                            Name
+                            Зелена група
                         </h2>
+                        <h5 className={styles.cardH5}>
+                            Зала Младост
+                        </h5>
 
                     </div>
 
                     <div className={styles.cardText}>
+                        <div className={styles.cardP}>
+                            <p >
+                                Създанена: 2018
+                            </p>
+                            <p >
+                                Брой хора: 80
+                            </p>
+                        </div>
 
-                        <p className={styles.cardP}>
-                            something
-                        </p>
 
-                        <Link to='#' className={styles.button}>
-                            Към зала Младост
-                            <span className={styles.materialSymbolsOutlined}>
-                                <i className="fa-solid fa-arrow-right"></i>
-                            </span>
-                        </Link>
+                        <div className={styles.buttons}>
+                            <Link to='#' className={styles.btn}>
+                                Към групата
+                                <span className={styles.materialSymbolsOutlined}>
+                                    <i className="fa-solid fa-arrow-right"></i>
+                                </span>
+                            </Link>
+                            <Link to='#' className={styles.btn}>
+                                Добави в любими
+                                <span className={styles.materialSymbolsOutlined}>
+                                    <i className="fa-solid fa-heart"></i>
+                                </span>
+                            </Link>
+                        </div>
+
 
                         {/* <Link className={styles.btnEdit} to='#'>Edit</Link> */}
 
