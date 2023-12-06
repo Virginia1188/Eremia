0c46d0370f318579a63c03a88c2e9f5cd5cff45e
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-12-06 11:42:55 +0200

    Get data for groups and studio from the server

--- client/src/components/Admin/StudioDetails/StudioDetails.jsx
+++ client/src/components/Admin/StudioDetails/StudioDetails.jsx
@@ -1,110 +1,102 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useContext, useEffect, useReducer, useState } from 'react';
+
 import styles from './StudioDetails.module.css';
-import Table from 'react-bootstrap/Table';
-// import {} from '../../../../'
+import AuthContext from '../../../contexts/authContext';
+import * as studioService from '../../../services/studioService';
+import * as groupService from '../../../services/groupService';
+import reducer from './studioReducer';
 
-export default function StudioDetails() {
 
+
+export default function StudioDetails({ }) {
+
+    const { } = useContext(AuthContext);
+    const [studio, setStudio] = useState({});
+    // const [groups, setGroup] = useState({});
+    const [groups, dispatch] = useReducer(reducer, []);
+    const { studioId } = useParams();
+
+    useEffect(() => {
+        studioService.getOne(studioId)
+            .then(setStudio);
+
+        groupService.getAll(studioId)
+            .then((result) => {
+                dispatch({
+                    type: 'GET_ALL_GROUPS',
+                    payload: result,
+                })
+            })
+
+    }, [studioId])
+    // console.log(groups);
     return (
         <div className={styles.main}>
 
-            <h2 className={styles.titleStudios} >Зала Младост</h2>
+            <h2 className={styles.titleStudios} >Зала {studio.name}</h2>
             <div className={styles.studioDetails}>
-                <p>св. кипрян 254</p>
-                <span>Цветомир</span>
+                <p>Адрес: {studio.address}</p>
+                <span>Ръководител: {studio.instructor}</span>
             </div>
-            <h4>Групи в зала Младост</h4>
+            <h4>Групи в зала {studio.name}</h4>
 
 
             {/* TODO add schedule */}
-            {/* <div className={styles.tableDiv}>
-                <h5>График на репетициите</h5>
-                <Table responsive="sm" className={styles.table}>
-                    <thead>
-                        <tr>
-                            <th>Час/Ден</th>
-                            <th>Понеделник</th>
-                            <th>Бторник</th>
-                            <th>Сряда</th>
-                            <th>Четвъртък</th>
-                            <th>Петък</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>18.30ч</td>
-                            <td>Червени</td>
-                            <td>Зелени</td>
-                            <td>Зелени</td>
-                            <td>Червени</td>
-                            <td>Зелени</td>
 
-                        </tr>
-                        <tr>
-                            <td>19.45ч</td>
-                            <td>Розови</td>
-                            <td>Жълти</td>
-                            <td>Розови</td>
-                            <td>Жълти</td>
-                            <td>Жълти</td>
-
-                        </tr>
-
-                    </tbody>
-                </Table>
-            </div> */}
-
-            <div className={styles.cards}>
+            {groups.map(group => (
+                <div className={styles.cards}>
 
 
-                <div className={styles.card} >
-                    <img className={styles.cardImg} src='https://scontent-lhr8-1.xx.fbcdn.net/v/t39.30808-6/384220642_705558081613582_1456725998677837583_n.jpg?stp=c52.0.206.206a_dst-jpg_p206x206&_nc_cat=111&ccb=1-7&_nc_sid=3d9721&_nc_ohc=CxfqeDrdzC8AX8TRgzM&_nc_ht=scontent-lhr8-1.xx&oh=00_AfCAcVS0V0HDVMAIqwl6JIpudgl4pcHlCmv69fdHKm1aIw&oe=657380F4' alt="logo" />
-                    <div >
-                        <h2 className={styles.cardH2}>
-                            Зелена група
-                        </h2>
-                        <h5 className={styles.cardH5}>
-                            Зала Младост
-                        </h5>
+                    <div className={styles.card} >
+                        <img className={styles.cardImg} src='https://scontent-lhr8-1.xx.fbcdn.net/v/t39.30808-6/384220642_705558081613582_1456725998677837583_n.jpg?stp=c52.0.206.206a_dst-jpg_p206x206&_nc_cat=111&ccb=1-7&_nc_sid=3d9721&_nc_ohc=CxfqeDrdzC8AX8TRgzM&_nc_ht=scontent-lhr8-1.xx&oh=00_AfCAcVS0V0HDVMAIqwl6JIpudgl4pcHlCmv69fdHKm1aIw&oe=657380F4' alt="logo" />
+                        <div >
+                            <h2 className={styles.cardH2}>
+                                {group.name}
+                            </h2>
+                            <h5 className={styles.cardH5}>
+                                Зала {studio.name}
+                            </h5>
+
+                        </div>
+
+                        <div className={styles.cardText}>
+                            <div className={styles.cardP}>
+                                <p >
+                                    Създанена: {group.created}
+                                </p>
+                                <p >
+                                    Брой хора: {group.dances}
+                                </p>
+                            </div>
+
+
+                            <div className={styles.buttons}>
+                                <Link to='#' className={styles.btn}>
+                                    Към групата
+                                    <span className={styles.materialSymbolsOutlined}>
+                                        <i className="fa-solid fa-arrow-right"></i>
+                                    </span>
+                                </Link>
+                                <Link to='#' className={styles.btn}>
+                                    Харесай
+                                    <span className={styles.materialSymbolsOutlined}>
+                                        <i className="fa-solid fa-heart"></i>
+                                    </span>
+                                </Link>
+                            </div>
+
+                            {/* <Link className={styles.btnEdit} to='#'>Харесай</Link> */}
+
+                        </div>
 
                     </div>
 
-                    <div className={styles.cardText}>
-                        <div className={styles.cardP}>
-                            <p >
-                                Създанена: 2018
-                            </p>
-                            <p >
-                                Брой хора: 80
-                            </p>
-                        </div>
-
-
-                        <div className={styles.buttons}>
-                            <Link to='#' className={styles.btn}>
-                                Към групата
-                                <span className={styles.materialSymbolsOutlined}>
-                                    <i className="fa-solid fa-arrow-right"></i>
-                                </span>
-                            </Link>
-                            <Link to='#' className={styles.btn}>
-                                Добави в любими
-                                <span className={styles.materialSymbolsOutlined}>
-                                    <i className="fa-solid fa-heart"></i>
-                                </span>
-                            </Link>
-                        </div>
-
-
-                        {/* <Link className={styles.btnEdit} to='#'>Edit</Link> */}
-
-
-                    </div>
 
                 </div>
+            ))}
 
 
-            </div>
 
         </div>
     );
